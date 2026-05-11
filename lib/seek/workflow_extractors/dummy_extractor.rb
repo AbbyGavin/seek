ca4ccb9d5255dd308b1fdf46af6228642ab1ffee
Abby Gavin <146940380+AbbyGavin@users.noreply.github.com>
2026-05-11 10:26:32 +0100

    Added initial dummy class

---
+++ lib/seek/workflow_extractors/dummy_extractor.rb
@@ -0,0 +1,21 @@
+require 'json'
+
+module Seek
+  module WorkflowExtractors
+    # Dummy class for testing. The argument should be a json string
+    class DummyExtractor < Base
+      attr_accessor :title, :description
+
+      def initialize(to_extract)
+        super
+        @to_extract = to_extract
+      end
+
+      def extract
+        j = JSON.parse(to_extract)
+        @title = j['title']
+        @description = j['description']
+      end
+    end
+  end
+end
