4638724782aa4aa7690fe252d6bbdf4445d919c0
Abby Gavin <146940380+AbbyGavin@users.noreply.github.com>
2026-05-12 14:48:00 +0100

    Adding file to test

--- test/unit/workflow_extraction/dummy_extractor_test.rb
+++ test/unit/workflow_extraction/dummy_extractor_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class DummyExtractorTest < ActiveSupport::TestCase
   test 'extracts metadata' do
-    json_string = '{ "title": "a title", "description": "some description", "other": "another property" }'
-    extractor = Seek::WorkflowExtractors::DummyExtractor.new(json_string)
+    wf = open_fixture_file('workflows/dummy_test.json')
+    extractor = Seek::WorkflowExtractors::DummyExtractor.new(wf)
     extractor.extract
 
     assert_equal 'a title', extractor.title
